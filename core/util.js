15b701a758c19f8809248b70513b4a478b82c370
firiz <f.sahafzadeh@gmail.com>
2015-07-03 18:57:46 +0430

    sendPhoto method added + oru command

--- core/util.js
+++ core/util.js
@@ -1,16 +1,39 @@
 var request = require('request');
+var http = require('http');
+var fs = require('fs');
 var jsonfile = require('jsonfile');
 var config = jsonfile.readFileSync('./config.json');
 var env = process.env.NODE_ENV;
 
 var functions = {
   sendMessage: function(data) {
-    if(config[env].url.snd) {
-      request.post(config[env].url.snd + 'sendMessage').form(data);
-    }
-    else {
-      console.log(data);
-    }
+    request.post(config[env].url.snd + 'sendMessage').form(data);
+  },
+  sendImageFromUrl: function(data) {
+    var ext = data.imageUrl.substr(data.imageUrl.length - 4);
+    var fileName = './temp/' + Math.floor((Math.random() * 999999)) + ext;
+    var file = fs.createWriteStream(fileName);
+
+    http.get(data.imageUrl, function (response) {
+      response.pipe(file);
+      file.on('finish', function () {
+        file.close(function() {
+          delete data.imageUrl;
+          var method;
+          if(ext === '.gif') {
+            method = 'sendDocument';
+            data.document = fs.createReadStream(fileName);
+          }
+          else {
+            method = 'sendPhoto';
+            data.photo = fs.createReadStream(fileName);
+          }
+          request.post({url: config[env].url.snd + method, formData: data}, function() {
+            fs.unlink(fileName);
+          });
+        });
+      });
+    });
   }
 };
 
