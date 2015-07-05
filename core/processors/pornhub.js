0859e58c994c3ed3a0bc2ef371aecebe3f7072c1
firiz <f.sahafzadeh@gmail.com>
2015-07-06 00:08:31 +0430

    added ph

--- core/processors/pornhub.js
+++ core/processors/pornhub.js
@@ -5,18 +5,29 @@ var util = require('./../util');
 var funstions = {
   execute: function (cmd) {
     var pageNumber = Math.floor((Math.random() * 4000));
-    var searchURL = 'http://www.pornhub.com/gifs?page=' + pageNumber;
+    var searchURL = 'http://www.pornhub.com/gifs?o=tr&page=' + pageNumber;
 
-    request(searchURL, function (error, response, results) {
+    var options = {
+      url: searchURL,
+      headers: {
+        'Host': 'www.pornhub.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:38.0) Gecko/20100101 Firefox/38.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate',
+        'DNT': '1',
+        'Referer': 'http://www.pornhub.com/gifs',
+        'Cookie': 'platform=pc; RNLBSERVERID=ded1684; performance_timing=other; FastPopSessionRequestNumber=4; local_storage=1',
+        'Connection': 'keep-alive'
+      }
+    };
+
+    request(options, function (error, response, results) {
       if(error) {
         console.log(error);
       }
 
-      data = {
-        chat_id: cmd.chat.id,
-        text: results
-      };
-      util.sendMessage(data);
+      console.log(results);
 
       var ch = cheerio.load(results);
       var thumbs = ch('img.thumb');
