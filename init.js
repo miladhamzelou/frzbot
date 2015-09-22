17a96a160a408d6928daba88039607c47acb0915
firiz <f.sahafzadeh@gmail.com>
2015-09-23 00:14:37 +0330

    init updated

--- init.js
+++ init.js
@@ -5,29 +5,27 @@ mongoose.connect('mongodb://localhost/frzbot');
 init();
 
 function init() {
-	var access = new Access({
-		_id: 'nsfw',
-  	cmds: ['porn', 'butts', 'boobs', 'dick', 'pussy', 'tattoo', 'nude', 'brunette', 'pale'],
-  	chatIds: [21826676]
-	});
-  access.save(function(err) {
-    if(err) {
+  var access = new Access({
+    _id: 'nsfw',
+    cmds: ['porn', 'butts', 'boobs', 'art', 'pussy', 'tattoo', 'nude', 'brunette', 'pale', 'pornhub', 'definebabe', 'lesbian', 'bikini', 'asian', 'pornstar'],
+    chatIds: [21826676]
+  });
+  access.save(function (err) {
+    if (err) {
       console.log(err);
-    }
-    else {
+    } else {
       console.log('inserted');
     }
   });
   var access2 = new Access({
-		_id: 'admin',
-  	cmds: ['addChatIdAccess', 'remChatIdAccess', 'addCmdAccess', 'remCmdAccess', 'allGroups', 'allUsers'],
-  	chatIds: [21826676]
-	});
-  access2.save(function(err) {
-    if(err) {
+    _id: 'admin',
+    cmds: ['addChatIdAccess', 'remChatIdAccess', 'addCmdAccess', 'remCmdAccess', 'allGroups', 'allUsers', 'commandsStats', 'broadcast'],
+    chatIds: [21826676]
+  });
+  access2.save(function (err) {
+    if (err) {
       console.log(err);
-    }
-    else {
+    } else {
       console.log('inserted');
     }
   });
